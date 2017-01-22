763ec1569adf7f0e73e3d0203e5f55ba5631f905
Faychan <nor.iffat32@gmail.com>
2017-01-22 22:34:08 +0700

    -Final

---
+++ TASK1_ASP/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using TASK1_ASP.Models.EntityManager;
+using TASK1_ASP.Models.ViewModel;
+
+namespace TASK1_ASP.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
